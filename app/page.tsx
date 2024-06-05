779db9f08c810c86df182d008abd778df7929086
sanalkumars <sanalskumar126@gmail.com>
2024-06-05 20:51:12 +0530

    bentogird part finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-red-400">hello portfolio</h1>
+        {/* <FloatingNav  navItems={navItems} /> */}
         <Hero />
+        <Grid />
       </div>
     </main>
   );
